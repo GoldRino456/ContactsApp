ae2756d6b6db715aad5dfae6b2ffe1f68f0767ec
Ethan E. <eastwoode375@gmail.com>
2025-07-10 21:33:20 -0500

    Renamed Solution + Starting on EF

---
+++ Contacts.GoldRino456/Data/ContactContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PhoneBook.GoldRino456.Data;
+public class ContactContext : DbContext
+{
+    public DbSet<ContactEntry> Contacts { get; set; }
+    public DbSet<ContactCategory> ContactCategories { get; set; }
+
+    public string DbPath { get; }
+
+    public ContactContext(string connectionString)
+    {
+        DbPath = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(DbPath);
+}
