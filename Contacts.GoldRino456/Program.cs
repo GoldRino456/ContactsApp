8b8b72d9db1bc8fce9387330829475a618058941
Ethan E. <eastwoode375@gmail.com>
2025-07-16 21:36:18 -0500

    Added Main Menu Options

--- Contacts.GoldRino456/Program.cs
+++ Contacts.GoldRino456/Program.cs
@@ -1,18 +1,67 @@
 ﻿using PhoneBook.GoldRino456;
 using PhoneBook.GoldRino456.Data;
+using System.Text.RegularExpressions;
+using Utilities.GoldRino456;
 
 class Program
 {
     static void Main()
     {
-        if(AppConfig.FetchConnectionString(out var connectionString))
+        if(!AppConfig.FetchConnectionString(out var connectionString))
         {
             throw new Exception("Could not find connection string.");
         }
 
         using var context = new ContactContext(connectionString);
 
-        //Display Menu Options
+        //TODO: Add Database Connection Check Here
+
+        bool isAppRunning = true;
+        while(isAppRunning)
+        {
+            var choice = DisplayMainMenu();
+
+            switch(choice)
+            {
+                case MenuOptions.CreateContact:
+                    break;
+
+                case MenuOptions.UpdateContact:
+                    break;
+
+                case MenuOptions.ViewContacts:
+                    break;
+
+                case MenuOptions.DeleteContact:
+                    break;
+
+                case MenuOptions.CreateCategory:
+                    break;
+
+                case MenuOptions.UpdateCategory:
+                    break;
+
+                case MenuOptions.DeleteCategory:
+                    break;
+
+                case MenuOptions.Quit:
+                    isAppRunning = false;
+                    break;
+
+            }
+        }
+    }
+
+    private static MenuOptions DisplayMainMenu()
+    {
+        Dictionary<string, int> menuOptionPairs = new();
+        foreach(var option in Enum.GetValues(typeof(MenuOptions)))
+        {
+            var displayText = Regex.Replace(option.ToString(), "(\\B[A-Z])", " $1"); //https://stackoverflow.com/questions/5796383/insert-spaces-between-words-on-a-camel-cased-token
+            menuOptionPairs.Add(displayText, (int)option);
+        }
+
+        return (MenuOptions) DisplayUtils.PromptUserForIndexSelection("What Would You Like To Do?", menuOptionPairs);
     }
 
     enum MenuOptions
